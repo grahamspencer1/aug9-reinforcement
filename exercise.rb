ff1285c906939b28f8c858ba5cbc1a9a3bd4c5a1
Graham Spencer <grahamspence@gmail.com>
2018-08-09 16:19:02 -0400

    blackjack and how she is played

--- exercise.rb
+++ exercise.rb
@@ -10,6 +10,19 @@ class Deck
 
 end
 
+class Dealer
+
+  def initialize
+    @cards_foe = [16, 17, 18, 19, 20, 21]
+  end
+
+  def deal_foe
+    @cards_foe.sample
+  end
+
+end
+
+
 class Hand
 
   def initialize
@@ -33,19 +46,28 @@ end
 
 def game
 
-  deck = Deck.new
+  player = Deck.new
+  foe = Dealer.new
   hand = Hand.new
+
+  to_beat = foe.deal_foe
+
   puts "Draw a card?"
   while hand.score <= 21
     answer = gets.chomp
     if answer.downcase == 'y'
-      hand.add_card(deck.deal)
+      hand.add_card(player.deal)
       puts "Your score is #{hand.score}. Draw again?"
     else
-      return "Your score is #{hand.score}."
+      puts "Your score is #{hand.score}. The dealer's score is #{to_beat}."
+      if hand.score > to_beat
+        return "You win!"
+      else
+        return "You lose!"
+      end
     end
   end
   return "You went over 21!"
 end
 
-p game
+puts game
